250731149c91bb61d069b71f2573fed3aadd6920
Ging1991 <carloscaballeromorel@gmail.com>
2024-12-09 18:06:32 -0300

    Commit inicial

---
+++ Lectores/LectorCadena.cs
@@ -0,0 +1,25 @@
+﻿namespace Ging1991.Persistencia.Lectores {
+		
+	public class LectorCadena : LectorGenerico<LectorCadena.Dato> {
+
+		public LectorCadena(string direccion, Tipo tipo, string direccionInicializacion = "") : base(direccion, tipo, direccionInicializacion) {}
+
+
+		public void Guardar(string valor) {
+			Dato dato = new Dato();
+			dato.valor = valor;
+			Guardar(dato);
+		}
+
+
+		[System.Serializable]
+		public class Dato {
+
+			public string valor;
+
+		}
+		
+
+	}
+
+}
